67eaa828347c772097148c18c80d9eab81512929
Pranjal <pranjaljsl2910@gmail.com>
2021-05-01 18:07:24 +0530

    basics

--- gfgCourse/src/basics/palindromeNumber.java
+++ gfgCourse/src/basics/palindromeNumber.java
@@ -6,14 +6,33 @@ public class palindromeNumber {
 	public static void main(String args[]) {
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
-		palin(n);
+		boolean ans = palin(n);
+		System.out.println(ans);
 	}
 
-	static void palin(int n) {
-		int ans = 0;
-		int count = (int) Math.log10(n) + 1;
-		while (count != 0) {
+	static boolean palin(int n) {
+		int rev = 0;
 
+		int temp = n;
+		while (temp != 0) {
+			int ld = temp % 10;
+
+			rev = rev * 10 + ld;
+
+			temp = temp / 10;
 		}
+
+		return rev == n;
+//		int rev = 0;
+//		int dup = n;
+//		int i = 1;
+//		while (dup != 0) {
+//
+//			rev = (rev * i) + (dup % 10);
+//			dup /= 10;
+//			i = 10;
+//		}
+//		System.out.println(rev);
+//		return (rev == n);
 	}
 }
