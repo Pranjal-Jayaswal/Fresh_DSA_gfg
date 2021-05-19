28708984a6de2b33ae820db67e2054f48740b200
Pranjal <pranjaljsl2910@gmail.com>
2021-05-19 09:11:55 +0530

    rotatedArray

--- gfgCourse/src/arrays/arrayList.java
+++ gfgCourse/src/arrays/arrayList.java
@@ -13,5 +13,14 @@ public class arrayList {
 		System.out.println(al);
 		System.out.println(al.get(1));
 		System.out.println(al.contains(20));
+
+//		.................................................................................................
+
+		long TIME_START;
+		long TIME_END;
+		TIME_START = System.currentTimeMillis();
+//		solve(sc, pw);
+		TIME_END = System.currentTimeMillis();
+		System.err.println("Time used: " + (TIME_END - TIME_START) + ".");
 	}
 }
