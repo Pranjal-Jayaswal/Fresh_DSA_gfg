5790b8b5590d7cc5a9c544dcec7d4794a29aa7ec
Pranjal <pranjaljsl2910@gmail.com>
2021-06-06 23:33:04 +0530

    stack

--- gfgCourse/src/arrays/majorityElement.java
+++ gfgCourse/src/arrays/majorityElement.java
@@ -7,6 +7,34 @@ public class majorityElement {
 	}
 
 	static int findMajority(int arr[], int n) {
+
+//		this commented wont work 
+//		read
+//		read
+
+//		int temp = arr[0], count = 1, ans = arr[0];
+//		for (int i = 1; i < arr.length; i++) {
+//			if (arr[i] == temp)
+//				++count;
+//			else {
+//				--count;
+//			}
+//			if (count == 0) {
+//				count = 1;
+//				temp = arr[i];
+//				ans = arr[i];
+//			}
+//			System.out.println(ans);
+//
+//			System.out.println(count);
+//			System.out.println(temp);
+//			System.out.println();
+//		}
+//
+//		if (count <= arr.length / 2)
+//			return -1;
+//
+//		return ans;
 		int res = 0, count = 1;
 		for (int i = 1; i < n; i++) {
 			if (arr[res] == arr[i])
