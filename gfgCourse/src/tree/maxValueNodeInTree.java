58f5896a1b78b1e005b00c85fb259caad87582b9
Pranjal <pranjaljsl2910@gmail.com>
2021-06-08 23:49:49 +0530

    treePart1

---
+++ gfgCourse/src/tree/maxValueNodeInTree.java
@@ -0,0 +1,20 @@
+package tree;
+
+public class maxValueNodeInTree {
+	public static void main(String args[]) {
+		Node root = new Node(20);
+		root.left = new Node(80);
+		root.right = new Node(30);
+		root.right.left = new Node(40);
+		root.right.right = new Node(50);
+
+		System.out.print(getMax(root));
+	}
+
+	public static int getMax(Node root) {
+		if (root == null)
+			return Integer.MIN_VALUE;
+		else
+			return Math.max(root.key, Math.max(getMax(root.left), getMax(root.right)));
+	}
+}
